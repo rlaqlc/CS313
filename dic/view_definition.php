adfaf05e06ab4d8ba812e2d905fa8a35fd045564
rlaqlc <rlaqlc@hotmail.com>
2015-05-25 00:38:59 -0600

    revised connection

--- dic/view_definition.php
+++ dic/view_definition.php
@@ -1,14 +1,14 @@
 <?php
 	try
 	{
-		define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
-		define('DB_PORT',getenv('OPENSHIFT_MYSQL_DB_PORT')); 
-		define('DB_USER',getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
-		define('DB_PASS',getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
-		$dbname = "wedefine";
+define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
+define('DB_PORT',getenv('OPENSHIFT_MYSQL_DB_PORT')); 
+define('DB_USER',getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
+define('DB_PASS',getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+define('DB_NAME',getenv('OPENSHIFT_GEAR_NAME'));
 
-		$dsn = 'mysql:dbname='.$dbname.';host='.DB_HOST.';port='.DB_PORT;
-		$sdb = new PDO($dsn, DB_USER, DB_PASS);
+$dsn = 'mysql:dbname='.DB_NAME.';host='.DB_HOST.';port='.DB_PORT;
+$dbh = new PDO($dsn, DB_USER, DB_PASS);
 	}
 	catch (PDOException $ex)
 	{
